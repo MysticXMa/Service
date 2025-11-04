0665fb8d844167aaf9f36cf9e3bcf3661192ebe6
Mystic <sdasdaasda44@gmail.com>
2025-11-04 21:25:36 +0200

    -

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import threading
 import time
@@ -218,4 +219,5 @@ def health():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
